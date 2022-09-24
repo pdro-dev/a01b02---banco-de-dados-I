2de82ec37f6fda674a24dcb15982befbb9e58c98
pss-ctd <pedro.simoes.soares@gmail.com>
2022-09-24 19:22:02 -0300

    procedure ainda bugado

--- checkpoint_03/cp3_procedure.sql
+++ checkpoint_03/cp3_procedure.sql
@@ -3,16 +3,32 @@
 
 */
 
--- 3. Crie uma procedure que, dado um número como parâmetro, retorne dados de duas ou mais tabelas.
+-- Procedures para retornar a quantidade de mesas atendidas por funcionario
 
-CREATE PROCEDURE cp3_procedure (IN numero INT)
+DELIMITER $$
+CREATE PROCEDURE qtdMesasAtendidas 
+(
+    IN NumFuncId INT
+)
 BEGIN
-    SELECT * FROM mesa WHERE mesaId = numero;
-    SELECT * FROM reserva WHERE reservaId = numero;
-    SELECT * FROM reservaMesa WHERE reservaMesaId = numero;
-    SELECT * FROM funcionarios WHERE funcionarioId = numero;
-    SELECT * FROM pedido WHERE pedidoId = numero;
-    SELECT * FROM item WHERE itemId = numero;
-    SELECT * FROM itemPedido WHERE itemPedidoId = numero;
-END
+    SELECT 
+    -- nome completo funcionario
+    CONCAT(f.nome, ' ', f.sobrenome) AS 'Nome Completo',
+    -- cargo funcionario
+    f.cargo AS 'Cargo',
+    -- quantidade de mesas atendidas
+    COUNT (m.mesaId) AS 'Mesas Atendidas'
+    -- quantidade de pedidos retirados
+    COUNT(p.pedidoId) AS 'Pedidos Retirados'
+    FROM funcionarios f
+    INNER JOIN pedido p ON p.funcionarioId = f.funcionarioId
+    WHERE f.funcionarioId = NumFuncId;
+END$$
+DELIMITER ;
 
+
+-- testando procedure
+CALL qtdMesasAtendidas(1);
+
+-- drop procedure
+DROP PROCEDURE qtdMesasAtendidas;
